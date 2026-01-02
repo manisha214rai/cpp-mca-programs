b89e2d26d9027e91bc450e533e693ad824d7e69a
Manisha Rai <rai.manisha2174@gmail.com>
2026-01-02 22:35:15 +0530

    Add hello world program demonstrating basic output using cout

---
+++ basics/basics/hello_world.cpp
@@ -0,0 +1,14 @@
+/*
+ * Program: Hello World
+ * Description: Basic C++ program to print a welcome message
+ * Author: Manisha
+ * Date: January 2, 2026
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, Manisha! Welcome to your MCA C++ repository!" << endl;
+    return 0;
+}
